d5ed2e2f3b4bae055c22e12d98403ed0617e1fef
GVlad <number10-95@bk.ru>
2022-12-19 19:49:15 +0300

    add README.md

--- StackOverflow.py
+++ StackOverflow.py
@@ -2,14 +2,17 @@ import requests
 from pprint import pprint
 import datetime
 
+n = int(input('Введите количество дней, за которые необходимо собрать данные: '))
+tags = input('Введите ключевое слово для поиска: ')
 current_date = datetime.datetime.today()
-from_date = int((datetime.datetime.today() - datetime.timedelta(days=2)).timestamp())
+from_date = int((datetime.datetime.today() - datetime.timedelta(days=n)).timestamp())
 params = {
     'fromdate': from_date,
     'order': 'desc',
-    'tagged': 'Python',
+    'tagged': tags,
     'site': 'stackoverflow'
 }
 url = 'https://api.stackexchange.com/2.3/questions?'
 res = requests.get(url, params=params).json()
 pprint(res)
+print(len(res['items']))
